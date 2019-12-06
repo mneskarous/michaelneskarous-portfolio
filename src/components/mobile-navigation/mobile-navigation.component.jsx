a2eeb812161a70938c8cc398854541ca54b98c56
Michael Eskarous <mneskarous@gmail.com>
2019-12-06 09:35:45 -0800

    Changed to class component

--- src/components/mobile-navigation/mobile-navigation.component.jsx
+++ src/components/mobile-navigation/mobile-navigation.component.jsx
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './mobile-navigation.styles.scss';
 
-const MobileNavigation = (props) => (
-  <div className="navbar">
-    <div className="menu">
-      <img src="menu.svg" alt="menu" />
-    </div>
-    <div className="image-links">
-      <a href="https://www.linkedin.com/in/michaelneskarous/">
-        <img src="linkedin.svg" alt="linkedin link" />
-      </a>
-      <a href="https://github.com/mneskarous">
-        <img src="github.svg" alt="github link" />
-      </a>
-      <a href="mailto:michaelneskarous@gmail.com">
-        <img src="email.svg" alt="email link" />
-      </a>
-    </div>
-  </div>
-);
+export default class MobileNavigation extends Component {
+  constructor() {
+    super();
 
-export default MobileNavigation;
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div className="navbar">
+        <div className="menu">
+          <img src="menu.svg" alt="menu" />
+        </div>
+        <div className="image-links">
+          <a href="https://www.linkedin.com/in/michaelneskarous/">
+            <img src="linkedin.svg" alt="linkedin link" />
+          </a>
+          <a href="https://github.com/mneskarous">
+            <img src="github.svg" alt="github link" />
+          </a>
+          <a href="mailto:michaelneskarous@gmail.com">
+            <img src="email.svg" alt="email link" />
+          </a>
+        </div>
+      </div>
+    )
+  }
+}
+
