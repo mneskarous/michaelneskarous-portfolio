7edf1d4595aa7762c2984b67bef2e3d07626f567
Michael Eskarous <mneskarous@gmail.com>
2019-12-05 16:58:08 -0800

    Created Intro component

---
+++ src/components/intro/intro.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Intro = () => (
+  <div className="intro">
+    <img className="portrait" src="./DSC_4122_small.jpg"  alt="Michael Eskarous" width="500"/>
+    <h2>Hi! I'm Mike and I'm a software engineer!</h2>
+  </div>
+);
+
+export default Intro;
