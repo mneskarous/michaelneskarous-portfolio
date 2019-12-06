7fac19949e263af85978f4061d2398aabf6e21b5
Michael Eskarous <mneskarous@gmail.com>
2019-12-06 11:25:02 -0800

    Removed width attribute from image

--- src/components/intro/intro.component.jsx
+++ src/components/intro/intro.component.jsx
@@ -4,7 +4,7 @@ import './intro.styles.scss';
 
 const Intro = () => (
   <div className="intro">
-    <img className="portrait" src="./DSC_4122_small.jpg"  alt="Michael Eskarous" width="500"/>
+    <img className="portrait" src="./DSC_4122_small.jpg"  alt="Michael Eskarous" />
     <h2>Hi! I'm Mike and I'm a software engineer!</h2>
   </div>
 );
