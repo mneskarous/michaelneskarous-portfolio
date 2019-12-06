56d613c9da1c5a59dac5e1d8e56e2bfc834ca765
Michael Eskarous <mneskarous@gmail.com>
2019-12-06 11:46:32 -0800

    Added styles

--- src/components/intro/intro.component.jsx
+++ src/components/intro/intro.component.jsx
@@ -4,8 +4,12 @@ import './intro.styles.scss';
 
 const Intro = () => (
   <div className="intro">
-    <img className="portrait" src="./DSC_4122_small.jpg"  alt="Michael Eskarous" />
-    <h2>Hi! I'm Mike and I'm a software engineer!</h2>
+    <div className="image-container">
+      <img className="portrait" src="./DSC_4122_small.jpg"  alt="Michael Eskarous" />
+    </div>
+    <div className="intro-container">
+      <h2>Hi! I'm Mike and I'm a software engineer!</h2> 
+    </div>
   </div>
 );
 
