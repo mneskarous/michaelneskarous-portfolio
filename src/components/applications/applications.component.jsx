8565fbedf5f8d7066a6c2c2083647f38510445cc
Michael Eskarous <mneskarous@gmail.com>
2019-12-05 17:01:18 -0800

    Created Applications component

---
+++ src/components/applications/applications.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Applications = () => (
+  <div className="applications">
+    <h2>Applications</h2>
+    <p>
+
+    </p>
+  </div>
+);
+
+export default Applications;
