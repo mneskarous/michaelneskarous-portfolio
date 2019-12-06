d5e7395e9582de8334299cf2f08c41f715411fab
Michael Eskarous <mneskarous@gmail.com>
2019-12-05 17:10:49 -0800

    Imported components into App component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import React from 'react';
+
+import Navbar from './components/navbar/navbar.component';
+import Intro from './components/intro/intro.component';
+import AboutMe from './components/about-me/about-me.component';
+import Skills from './components/skills/skills.component';
+import Applications from './components/applications/applications.component';
+import ContactForm from './components/contact-form/contact-form.component';
+
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="navbar-container">
+        <Navbar />
+      </div>
+      <div className="content">
+        <div className="intro-container">
+          <Intro />
+        </div>
+        <div className="about-me-container">
+          <AboutMe />
+        </div>
+        <div className="skills-container">
+          <Skills />
+        </div>
+        <div className="applications-container">
+          <Applications />
+        </div>
+        <div className="contact-form-container">
+          <ContactForm />
+        </div>
+      </div>
     </div>
   );
 }
