4a3e3c864865fb4b9493e803371b8dd1a67fa81e
Michael Eskarous <mneskarous@gmail.com>
2019-12-06 10:39:45 -0800

    Added and styled dropdown menu for mobile and modified navigation styles

--- src/components/mobile-navigation/mobile-navigation.component.jsx
+++ src/components/mobile-navigation/mobile-navigation.component.jsx
@@ -7,17 +7,38 @@ export default class MobileNavigation extends Component {
     super();
 
     this.state = {
-
+      hidden: true
     }
   }
 
+  handleClick = event => {
+    const { hidden } = this.state;
+    event.preventDefault();
+    this.setState({
+      hidden: !hidden
+    });
+  }
+
   render() {
+    const { hidden } = this.state;
     return (
       <div className="navbar">
         <div className="menu">
-          <img src="menu.svg" alt="menu" />
+          <img onClick={this.handleClick} src="menu.svg" alt="menu" />
         </div>
-        <div className="image-links">
+        <div>
+        {!hidden ? ( 
+          <div className="vertical-text-links">
+            <a href="#about">About Me</a>
+            <a href="#skills">Skills</a>
+            <a href="#Applications">Applications</a>
+            <a href="#contact-form">Contact Form</a>
+            <a href="#resume">Resume</a>
+          </div> ) : (
+          null
+        )}
+        </div>
+        <div className="mobile-image-links">
           <a href="https://www.linkedin.com/in/michaelneskarous/">
             <img src="linkedin.svg" alt="linkedin link" />
           </a>
