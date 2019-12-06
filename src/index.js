a09577ef6c6f789d2a853dbe0d02532bba394a52
Michael Eskarous <mneskarous@gmail.com>
2019-12-05 17:10:13 -0800

    Imported React-Breakpoints

--- src/index.js
+++ src/index.js
@@ -2,9 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ReactBreakPoints from 'react-breakpoints';
 // import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const breakpoints = {
+  mobile: 320,
+  mobileLandscape: 480,
+  tablet: 768,
+  tabletLandscape: 1024,
+  desktop: 1200,
+  desktopLarge: 1500,
+  desktopWide: 1920,
+}
+
+ReactDOM.render(
+  <ReactBreakPoints breakpoints={breakpoints}>
+    <App />
+  </ReactBreakPoints>, 
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
