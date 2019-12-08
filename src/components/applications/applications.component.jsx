e812c947709e115ea05239a1379d551fd3d67f96
Michael Eskarous <mneskarous@gmail.com>
2019-12-08 15:10:17 -0800

    Created Application component and added primary styles

--- src/components/applications/applications.component.jsx
+++ src/components/applications/applications.component.jsx
@@ -4,10 +4,23 @@ import './applications.styles.scss';
 
 const Applications = () => (
   <div id="applications" className="applications">
-    <h2>Applications</h2>
-    <p>
-
-    </p>
+    <div className="header-container">
+      <h2>Applications</h2>
+    </div>
+    <div className="applications-container">
+      <div className="application">
+        <div>Fitness Tracker: A responsive, mobile-first web app that allows users to keep track of their fitness progress in various exercise categories</div>
+      </div>
+      <div className="application">
+        <div>LalaLime Review Section: A functional front-end replica of the LuluLemon Review Section</div>
+      </div>
+      <div className="application">
+      <div>Anthropologie Main Product Section: System design mock-up of the Anthropologie Main Product Section</div>
+      </div>
+      <div className="application">
+      <div>Memory Game: A game that allows players to use their short-term memory to match cards to their corresponding match</div>
+      </div>
+    </div>
   </div>
 );
 
