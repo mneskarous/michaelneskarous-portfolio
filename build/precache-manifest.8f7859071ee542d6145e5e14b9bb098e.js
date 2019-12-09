b6de52e7ca7688c24bb5ff20e6abce6bb7d76801
Michael Eskarous <mneskarous@gmail.com>
2019-12-09 12:08:02 -0800

    Added files for github pages

---
+++ build/precache-manifest.8f7859071ee542d6145e5e14b9bb098e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30c6f4811a59aca27d8ab8d8c0c44648",
+    "url": "/michaelneskarous-portfolio/index.html"
+  },
+  {
+    "revision": "d0987fbbe22b64266cac",
+    "url": "/michaelneskarous-portfolio/static/css/2.853ab78a.chunk.css"
+  },
+  {
+    "revision": "c6055100829fea30c875",
+    "url": "/michaelneskarous-portfolio/static/css/main.afc4b450.chunk.css"
+  },
+  {
+    "revision": "d0987fbbe22b64266cac",
+    "url": "/michaelneskarous-portfolio/static/js/2.c3a74fb1.chunk.js"
+  },
+  {
+    "revision": "f0f78813f10b0fa6934e0b67cbb196b8",
+    "url": "/michaelneskarous-portfolio/static/js/2.c3a74fb1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c6055100829fea30c875",
+    "url": "/michaelneskarous-portfolio/static/js/main.3bc3ad1c.chunk.js"
+  },
+  {
+    "revision": "22c0d182770bd7d7af01",
+    "url": "/michaelneskarous-portfolio/static/js/runtime-main.cf408548.js"
+  }
+]);
