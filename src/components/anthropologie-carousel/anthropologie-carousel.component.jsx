aba6f939edb204b14eb95e66f7f50296458709d2
Michael Eskarous <mneskarous@gmail.com>
2019-12-08 23:00:29 -0800

    Converted to functional component and cleaned code syntax

--- src/components/anthropologie-carousel/anthropologie-carousel.component.jsx
+++ src/components/anthropologie-carousel/anthropologie-carousel.component.jsx
@@ -1,26 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
 
 import 'pure-react-carousel/dist/react-carousel.es.css';
 import '../applications/carousel.styles.scss';
 
-export default class AnthropologieCarousel extends Component {
-    render() {
-      return(
-        <div className="carousel-container">
-          <CarouselProvider
-            naturalSlideWidth={150}
-            naturalSlideHeight={107}
-            totalSlides={2}
-          >
-            <Slider className="carousel">
-              <Slide className="image-container" index={0}><img className="image" src='./images/anthropolgie/one-service-get.png' alt="Before horizontal scaling"/></Slide>
-              <Slide className="image-container" index={1}><img className="image" src='./images/anthropolgie/five-services-get.png' alt="After horizontal scaling" /></Slide>
-            </Slider>
-            <ButtonBack className="prev-arrow">&#60;</ButtonBack>
-            <ButtonNext className="next-arrow">&#62;</ButtonNext>
-          </CarouselProvider>
-        </div>
-      )
-    }
-}
+const AnthropologieCarousel = () => (
+  <div className="carousel-container">
+    <CarouselProvider
+      naturalSlideWidth={150}
+      naturalSlideHeight={107}
+      totalSlides={2}
+    >
+      <Slider className="carousel">
+        <Slide className="image-container" index={0}>
+          <img className="image" src='./images/anthropolgie/one-service-get.png' alt="Before horizontal scaling"/>
+        </Slide>
+        <Slide className="image-container" index={1}>
+          <img className="image" src='./images/anthropolgie/five-services-get.png' alt="After horizontal scaling" />
+        </Slide>
+      </Slider>
+      <ButtonBack className="prev-arrow">&#60;</ButtonBack>
+      <ButtonNext className="next-arrow">&#62;</ButtonNext>
+    </CarouselProvider>
+  </div>
+);
+
+export default AnthropologieCarousel;
