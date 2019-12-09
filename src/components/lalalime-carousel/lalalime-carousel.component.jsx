0ec5650f09ba06dc7df71b1179c1ed96e405a0fc
Michael Eskarous <mneskarous@gmail.com>
2019-12-08 22:53:48 -0800

    Converted to functional component and cleaned code syntax

--- src/components/lalalime-carousel/lalalime-carousel.component.jsx
+++ src/components/lalalime-carousel/lalalime-carousel.component.jsx
@@ -1,29 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
 
 import 'pure-react-carousel/dist/react-carousel.es.css';
 import '../applications/carousel.styles.scss';
 
-export default class LalalimeCarousel extends Component {
-    render() {
-      return(
-        <div className="carousel-container">
-          <CarouselProvider
-            naturalSlideWidth={80}
-            naturalSlideHeight={51}
-            totalSlides={3}
-          >
-            <Slider className="carousel">
-              <Slide className="image-container" index={0}><img className="image" src='./images/lalalime/FEC-demo-filter.gif' alt="Lalalime filter demo"/></Slide>
-              <Slide className="image-container" index={1}><img className="image" src='./images/lalalime/FEC-demo-popups.gif' alt="Lalalime popups demo" /></Slide>
-              <Slide className="image-container"index={2}><img className="image" src='./images/lalalime/FEC-Demo-Sort.gif' alt=" Lalalime sort demo" /></Slide>
-            </Slider>
-            <ButtonBack className="prev-arrow">&#60;</ButtonBack>
-            <ButtonNext className="next-arrow">&#62;</ButtonNext>
-          </CarouselProvider>
-        </div>
-      )
-    
-    }
-  
-}
+const LalalimeCarousel = () => (
+  <div className="carousel-container">
+    <CarouselProvider
+      naturalSlideWidth={80}
+      naturalSlideHeight={51}
+      totalSlides={3}
+    >
+      <Slider className="carousel">
+        <Slide className="image-container" index={0}>
+          <img className="image" src='./images/lalalime/FEC-demo-filter.gif' alt="Lalalime filter demo"/>
+        </Slide>
+        <Slide className="image-container" index={1}>
+          <img className="image" src='./images/lalalime/FEC-demo-popups.gif' alt="Lalalime popups demo" />
+        </Slide>
+        <Slide className="image-container"index={2}>
+          <img className="image" src='./images/lalalime/FEC-Demo-Sort.gif' alt=" Lalalime sort demo" />
+        </Slide>
+      </Slider>
+      <ButtonBack className="prev-arrow">&#60;</ButtonBack>
+      <ButtonNext className="next-arrow">&#62;</ButtonNext>
+    </CarouselProvider>
+  </div>
+);
+
+export default LalalimeCarousel;
