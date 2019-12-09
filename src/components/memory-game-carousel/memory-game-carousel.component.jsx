f7393bb64dced98ab4094ea474d3435ade57d914
Michael Eskarous <mneskarous@gmail.com>
2019-12-08 22:57:56 -0800

    Converted to functional component and cleaned code syntax

--- src/components/memory-game-carousel/memory-game-carousel.component.jsx
+++ src/components/memory-game-carousel/memory-game-carousel.component.jsx
@@ -4,26 +4,28 @@ import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-re
 import 'pure-react-carousel/dist/react-carousel.es.css';
 import '../applications/carousel.styles.scss';
 
-export default class MemoryGameCarousel extends Component {
-    render() {
-      return(
-        <div className="carousel-container">
-          <CarouselProvider
-            naturalSlideWidth={100}
-            naturalSlideHeight={109}
-            totalSlides={3}
-          >
-            <Slider className="carousel">
-              <Slide className="image-container" index={0}><img className="image" src='./images/memory-game/memory-game-demo-1.gif' alt="Memory Game demo 1"/></Slide>
-              <Slide className="image-container" index={1}><img className="image" src='./images/memory-game/memory-game-demo-2.gif' alt="Memory Game demo 2" /></Slide>
-              <Slide className="image-container" index={2}><img className="image" src='./images/memory-game/memory-game-demo-3.gif' alt="Memory Game demo 3" /></Slide>
-            </Slider>
-            <ButtonBack className="prev-arrow">&#60;</ButtonBack>
-            <ButtonNext className="next-arrow">&#62;</ButtonNext>
-          </CarouselProvider>
-        </div>
-      )
-    
-    }
-  
-}
+const MemoryGameCarousel = () => (
+  <div className="carousel-container">
+    <CarouselProvider
+      naturalSlideWidth={100}
+      naturalSlideHeight={109}
+      totalSlides={3}
+    >
+      <Slider className="carousel">
+        <Slide className="image-container" index={0}>
+          <img className="image" src='./images/memory-game/memory-game-demo-1.gif' alt="Memory Game demo 1"/>
+        </Slide>
+        <Slide className="image-container" index={1}>
+          <img className="image" src='./images/memory-game/memory-game-demo-2.gif' alt="Memory Game demo 2" />
+        </Slide>
+        <Slide className="image-container" index={2}>
+          <img className="image" src='./images/memory-game/memory-game-demo-3.gif' alt="Memory Game demo 3" />
+        </Slide>
+      </Slider>
+      <ButtonBack className="prev-arrow">&#60;</ButtonBack>
+      <ButtonNext className="next-arrow">&#62;</ButtonNext>
+    </CarouselProvider>
+  </div>
+);
+
+export default MemoryGameCarousel;
