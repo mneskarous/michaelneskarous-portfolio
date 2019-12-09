2cd166b63cf9434190691d0db5d08424cfb87e05
Michael Eskarous <mneskarous@gmail.com>
2019-12-09 10:45:42 -0800

    Changed content

--- src/components/about-me/about-me.component.jsx
+++ src/components/about-me/about-me.component.jsx
@@ -12,7 +12,7 @@ const AboutMe = () => (
         My name is Mike, and I'm a software engineer with an unbridled enthusiasm for front-end developement. One of the things I love most about coding is that you get to be both technical and creative at the same time. I also love building apps and interfaces that people can interact with and enjoy. 
       </p>
       <p>
-        I'm always trying to learn new technologies and perfect my skills with the technologies I've already learned. 
+        I'm always trying to learn new technologies and perfect my skills with the technologies I've already learned. I welcome challenging tasks that test the limits of my problem-solving and analytical skills and my creative ability. 
       </p>
       <p>
         When I'm not coding, I like to spend time with my ever-patient wife and my friends and family, do volunteer work with my church, play games on my PS4, go to the gym, read, solve crossword puzzles, watch my favorite teams play, and watch good TV shows and movies.
