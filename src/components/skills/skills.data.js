3ec492e5907e0004928c2958cc5946b15be3961d
Michael Eskarous <mneskarous@gmail.com>
2019-12-09 12:30:07 -0800

    Changed to consistent case for image names

--- src/components/skills/skills.data.js
+++ src/components/skills/skills.data.js
@@ -1,13 +1,13 @@
 const SKILLS_DATA = [
   {
     id: 1,
-    imageUrl: "./images/JavaScript.png",
+    imageUrl: "./images/javascript.png",
     alt: "JavaScript logo",
     answer: 'JavaScript'
   },
   {
     id: 2,
-    imageUrl: "./images/HTML5.png",
+    imageUrl: "./images/html5.png",
     alt: "HTML5 logo",
     answer: 'HTML5'
   },
@@ -19,67 +19,67 @@ const SKILLS_DATA = [
   },
   {
     id: 4,
-    imageUrl: "./images/Sass.png",
+    imageUrl: "./images/sass.png",
     alt: "Sass logo",
     answer: 'Sass'
   },
   {
     id: 5,
-    imageUrl: "./images/React.svg.png",
+    imageUrl: "./images/react.svg.png",
     alt: "React logo",
     answer: 'React'
   },
   {
     id: 6,
-    imageUrl: "./images/Redux.svg",
+    imageUrl: "./images/redux.svg",
     alt: "Redux logo",
     answer: 'Redux'
   },
   {
     id: 7,
-    imageUrl: "./images/jQuery.svg",
+    imageUrl: "./images/jquery.svg",
     alt: "jQuery logo",
     answer: 'jQuery'
   },
   {
     id: 8,
-    imageUrl: "./images/NodeJS.svg",
+    imageUrl: "./images/nodejs.svg",
     alt: "NodeJS logo",
     answer: 'NodeJS'
   },
   {
     id: 9,
-    imageUrl: "./images/MongoDB.png",
+    imageUrl: "./images/mongodb.png",
     alt: "MongoDB logo",
     answer: 'MongoDB'
   },
   {
     id: 10,
-    imageUrl: "./images/MySQL.png",
+    imageUrl: "./images/mysql.png",
     alt: "MySQL logo",
     answer: 'MySQL'
   },
   {
     id: 11,
-    imageUrl: "./images/PostgreSQL.svg.png",
+    imageUrl: "./images/postgresql.svg.png",
     alt: "PostgreSQL logo",
     answer: 'PostgreSQL'
   },
   {
     id: 12,
-    imageUrl: "./images/Jest.png",
+    imageUrl: "./images/jest.png",
     alt: "Jest logo",
     answer: 'Jest'
   },
   {
     id: 13,
-    imageUrl: "./images/Git.png",
+    imageUrl: "./images/git.png",
     alt: "Git logo",
     answer: 'Git'
   },
   {
     id: 14,
-    imageUrl: "./images/npm-2.svg",
+    imageUrl: "./images/npm.svg",
     alt: "npm logo",
     answer: 'npm'
   },
