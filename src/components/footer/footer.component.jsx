8844579e53fd1ad9b78a2e31de8d8d7c28b6d289
Michael Eskarous <mneskarous@gmail.com>
2019-12-16 13:40:25 -0800

    Added image links to footer

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -4,7 +4,20 @@ import './footer.styles.scss';
 
 const Footer = () => (
   <div className="footer">
+    <div className="copyright">
     	&copy; 2019 Design & build by Michael Eskarous
+    </div>
+    <div className="footer-image-links">
+      <a href="https://www.linkedin.com/in/michaelneskarous/">
+        <img src="./images/linkedin.svg" alt="linkedin link" />
+      </a>
+      <a href="https://github.com/mneskarous">
+        <img src="./images/github.svg" alt="github link" />
+      </a>
+      <a href="mailto:michaelneskarous@gmail.com">
+        <img src="./images/email.svg" alt="email link" />
+      </a>
+    </div>
   </div>
 );
 
