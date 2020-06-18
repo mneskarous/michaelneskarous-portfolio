703531a36d2b6bc6029ca79ecbbf9807ed5f8eb7
Michael Eskarous <mneskarous@gmail.com>
2020-06-17 23:13:13 -0700

    Added Build folder

---
+++ build/precache-manifest.6d01857a37e83144d3f9a94f43433b4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de38bd4e051bb5169ebb441b05aa11dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6a6f733959e86eab474",
+    "url": "/static/css/2.853ab78a.chunk.css"
+  },
+  {
+    "revision": "5b1903bbba8b9c3b9011",
+    "url": "/static/css/main.65b14ee3.chunk.css"
+  },
+  {
+    "revision": "a6a6f733959e86eab474",
+    "url": "/static/js/2.45570d9d.chunk.js"
+  },
+  {
+    "revision": "f0f78813f10b0fa6934e0b67cbb196b8",
+    "url": "/static/js/2.45570d9d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5b1903bbba8b9c3b9011",
+    "url": "/static/js/main.bf62eddb.chunk.js"
+  },
+  {
+    "revision": "f20b41838ba111a18eb7",
+    "url": "/static/js/runtime-main.9d25e592.js"
+  }
+]);
